bcf9852be2db59e9c41f278ddb247c12dbc5f3d4
keerthikmr <keerthikumar.m10@gmail.com>
2024-09-01 00:44:11 +0530

    split and parse

--- login.js
+++ login.js
@@ -12,11 +12,14 @@ sign_in_btn.addEventListener("click", () => {
 
 function onSignIn(googleUser) {
   console.log("reached");
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log("Name: " + profile.getName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+  const tokens = googleUser.credential.split(".");
+  const responsePayload = JSON.parse(atob(tokens[1]));
+  console.log("ID: " + responsePayload.sub);
+  console.log("Full Name: " + responsePayload.name);
+  console.log("Given Name: " + responsePayload.given_name);
+  console.log("Family Name: " + responsePayload.family_name);
+  console.log("Image URL: " + responsePayload.picture);
+  console.log("Email: " + responsePayload.email);
 }
 
 const { OAuth2Client } = require("google-auth-library");
