bea5f4782aa6383e843a97f2ad5863d5c8b07551
keerthikmr <keerthikumar.m10@gmail.com>
2024-09-01 00:36:40 +0530

    using button

--- login.js
+++ login.js
@@ -18,3 +18,23 @@ function onSignIn(googleUser) {
   console.log("Image URL: " + profile.getImageUrl());
   console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
+
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(
+  "423329562434-dsn9edh8mpo8q5q3tfovbsmt33tgrmfc.apps.googleusercontent.com"
+);
+
+async function verifyToken(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience:
+      "423329562434-dsn9edh8mpo8q5q3tfovbsmt33tgrmfc.apps.googleusercontent.com",
+  });
+  const payload = ticket.getPayload();
+  const userid = payload["sub"];
+  console.log("User ID: ", userid);
+  // Further processing...
+}
+
+// Use the function
+verifyToken("ID_TOKEN_RECEIVED_FROM_CLIENT");
