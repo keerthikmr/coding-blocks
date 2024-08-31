0c556b273966cb6c1e5ac7276f7088bd10a48343
keerthikmr <keerthikumar.m10@gmail.com>
2024-09-01 00:00:13 +0530

    remove unwanted import

--- login.js
+++ login.js
@@ -1,5 +1,3 @@
-import jwt_decode from "jwt-decode";
-
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
@@ -11,11 +9,6 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
-
-function credentialHandler(response) {
-  var decoded = jwt_decode(response);
-  console.log(decoded);
-}
 
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
